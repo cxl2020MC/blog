f3b1e542569eb9265908bcd52bf6890cc3d8d77c
cxl2020MC <andamu2019@163.com>
2022-05-28 20:24:27 +0800

    git update

--- source/js/mirror.js
+++ source/js/mirror.js
@@ -1,10 +1,11 @@
 function mirrorwelcome(){
     var domain = document.domain
     console.log("你正在访问:"+domain)
-    if(domain.referrer!=='www.cxl2020mc.top'){
+    if(domain!=='www.cxl2020mc.top'){
         alert("你正在访问 陈鑫磊的博客 镜像站，主站域名www.cxl2020mc.top")
     }
 }
-$(document).ready(()=>{
-    mirrorwelcome()
-})
+//$(document).ready(()=>{
+    //mirrorwelcome()
+//})
+window.addEventListener("load", mirrorwelcome());

94b397c38e032916ef479cc268128a98ae172ef9
cxl2020MC <andamu2019@163.com>
2022-06-26 18:39:21 +0800

    git update

--- source/js/other.js
+++ source/js/other.js
@@ -1,21 +1,26 @@
-function mirrorwelcome(){
+function mirrorwelcome() {
     var domain = document.domain
-    console.log("你正在访问:"+domain)
-    if(domain!=='www.cxl2020mc.top'){
+    console.log("你正在访问:" + domain)
+    if (domain !== 'www.cxl2020mc.top') {
         alert("你正在访问 陈鑫磊的博客 镜像站，主站域名www.cxl2020mc.top")
     }
 }
 //$(document).ready(()=>{
-    //mirrorwelcome()
+//mirrorwelcome()
 //})
 
 //window.addEventListener("load", mirrorwelcome());
 
 //当一个文档完全下载到浏览器中时，才会触发window.onload事件
 //window.onload=function(){
-    　　　　　　　//dosth//你要做的事情
-    　　　　//}
+//dosth//你要做的事情
+//}
 
-window.addEventListener("onload", mirrorwelcome());
+document.onreadystatechange = function () 　　//当页面加载状态改变的时候执行function
+{
+    if (document.readyState == "complete") { 　　//当页面加载状态为完全结束时进入 
+        mirrorwelcome();
+        btf.snackbarShow('欢迎来到陈鑫磊的博客！'); 　　 //你要做的操作。
+    }
+}
 //btf.snackbarShow('欢迎来到陈鑫磊的博客！') 
-window.addEventListener("onload", btf.snackbarShow('欢迎来到陈鑫磊的博客！') );

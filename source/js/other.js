8fb457ae6385cd5024d9e8f04ddf52437a6d5cd0
cxl2020MC <andamu2019@163.com>
2022-06-27 20:23:16 +0800

    git update

--- source/js/other.js
+++ source/js/other.js
@@ -9,13 +9,21 @@ function mirrorwelcome() {
 //mirrorwelcome()
 //})
 
+function welcome() {
+    var url = window.location.pathname;
+    console.log("你正在访问路径:" + domain)
+    if (url == '/') {
+        btf.snackbarShow('欢迎来到陈鑫磊的博客！');
+    }
+}
+
 window.addEventListener("load", mirrorwelcome());
 
 document.onreadystatechange = function () 　　//当页面加载状态改变的时候执行function
 {
     if (document.readyState == "complete") { 　　//当页面加载状态为完全结束时进入 
         //mirrorwelcome();
-        btf.snackbarShow('欢迎来到陈鑫磊的博客！'); 　　 //你要做的操作。
+        welcome() 　　 //你要做的操作。
     }
 }
 //btf.snackbarShow('欢迎来到陈鑫磊的博客！') 

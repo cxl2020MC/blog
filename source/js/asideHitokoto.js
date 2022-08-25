de42cc13d1b2ebbe4e6dc8b0bfcfbd486b0b1e2a
cxl2020MC <andamu2019@163.com>
2022-08-25 21:00:53 +0800

    vscode

--- source/js/asideHitokoto.js
+++ source/js/asideHitokoto.js
@@ -1,7 +1,10 @@
-hitokotojson = await (await fetch('https://v1.hitokoto.cn?select=json')).json()
-hitokototext = hitokotojson['hitokoto']
-hitokotofrom = hitokotojson['from']
-hitokotostr = "『" + hitokototext + "』" + " —— " + hitokotofrom
-console.log("一言文本: " + hitokotostr)
-asideHitokoto = document.getElementById("asideHitokoto")
-asideHitokoto.innerHTML = hitokotostr
+// hitokotojson = await (await fetch('https://v1.hitokoto.cn?select=json')).json()
+fetch().then(res=>{
+    hitokotojson = res.json();
+    hitokototext = hitokotojson['hitokoto']
+    hitokotofrom = hitokotojson['from']
+    hitokotostr = "『" + hitokototext + "』" + " —— " + hitokotofrom
+    console.log("一言文本: " + hitokotostr)
+    asideHitokoto = document.getElementById("asideHitokoto")
+    asideHitokoto.innerHTML = hitokotostr
+})

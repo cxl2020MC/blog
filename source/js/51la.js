eaf2a582dde3eebbf42b42dc16801fd2d7331106
cxl2020MC <andamu2019@163.com>
2022-09-01 17:10:31 +0800

    vscode

---
+++ source/js/51la.js
@@ -0,0 +1,5 @@
+importScripts("//sdk.51.la/js-sdk-pro.min.js")
+LA.init({id: "Jnkp8oCL537VDXz1",ck: "Jnkp8oCL537VDXz1"})
+
+importScripts("//sdk.51.la/perf/js-sdk-perf.min.js")
+new LingQue.Monitor().init({id:"JnkoLfdriTFL8Kgt"});

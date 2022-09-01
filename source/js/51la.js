9b70e779ebaf1b67a72a648acde84de60112158d
cxl2020MC <andamu2019@163.com>
2022-09-01 19:33:57 +0800

    vscode

--- source/js/51la.js
+++ source/js/51la.js
@@ -1,10 +1,4 @@
-!function (p) { "use strict"; !function (t) { var s = window, e = document, i = p, c = "".concat("https:" === e.location.protocol ? "https://" : "http://", "sdk.51.la/js-sdk-pro.min.js"), n = e.createElement("script"), r = e.getElementsByTagName("script")[0]; n.type = "text/javascript", n.setAttribute("charset", "UTF-8"), n.async = !0, n.src = c, n.id = "LA_COLLECT", i.d = n; var o = function () { s.LA.ids.push(i) }; s.LA ? s.LA.ids && o() : (s.LA = p, s.LA.ids = [], o()), r.parentNode.insertBefore(n, r) }() }({ id: "Jnkp8oCL537VDXz1", ck: "Jnkp8oCL537VDXz1" });
-
-!(function (c, i, e, b) {
-    var h = i.createElement("script");
-    var f = i.getElementsByTagName("script")[0];
-    h.type = "text/javascript";
-    h.crossorigin = true;
-    h.onload = function () { new c[b]["Monitor"]().init({ id: "JnkoLfdriTFL8Kgt" }); };
-    f.parentNode.insertBefore(h, f); h.src = e;
-})(window, document, "https://sdk.51.la/perf/js-sdk-perf.min.js", "LingQue");
+//51.1a
+LA.init({id: "Jnkp8oCL537VDXz1",ck: "Jnkp8oCL537VDXz1"})
+//灵雀应用监控
+new LingQue.Monitor().init({id:"JnkoLfdriTFL8Kgt"});

1a6162ba699632668921a06cc71be834e8cddc33
cxl2020MC <andamu2019@163.com>
2022-09-01 13:57:16 +0800

    vscode

--- scripts/diytag.js
+++ scripts/diytag.js
@@ -4,12 +4,12 @@ function dplayer (args, content) {
     let p0 = args[0]
     let p1 = args[1]?args[1]:p0
     html = `<div id="${p1}"></div>
-    <script>
+<script>
     const ${p0} = new DPlayer({
         container: document.getElementById('${p1}'),
         ${content}
     });
-    </script>`
+</script>`
     return html
 }
 //标签注册函数

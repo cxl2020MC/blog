0f247273ce3b8330a8f3ac4e85bd8180c193d938
cxl2020MC <andamu2019@163.com>
2022-09-02 09:33:52 +0800

    vscode

--- scripts/diytag.js
+++ scripts/diytag.js
@@ -3,7 +3,8 @@ function dplayer (args, content) {
     args = args.join(' ').split(',')
     let p0 = args[0]
     let p1 = args[1]?args[1]:p0
-    html = `<div id="${p1}"></div>
+    html = `${hexo.render.renderSync({ text: '> 这里有个视频播放器，如无法显示请刷新', engine: 'markdown' })}
+<div id="${p1}"></div>
 <script>
     const ${p0} = new DPlayer({
         container: document.getElementById('${p1}'),

1bac39f4b892a9788f8f4fe079d71303b5db7459
cxl2020MC <andamu2019@163.com>
2022-09-07 16:04:42 +0800

    git update

--- source/js/copyright_ip.js
+++ source/js/copyright_ip.js
@@ -1,3 +1,13 @@
-fetch("https://api.cxl2020mc.top/ip/").then(res => res.json()).then(function(data){
-  
+fetch("https://api.cxl2020mc.top/ip/").then(res => res.json()).then(function (data) {
+    copyright_ip = document.getElementById("copyright_ip");
+    copyright_pos = document.getElementById("copyright_pos");
+    copyright_isp = document.getElementById("copyright_isp");
+    ip = data['data']['ip'];
+    city = data['data']['city'];
+    province = data['data']['province'];
+    isp = data['data']['isp'];
+    pos = `${ip} ${city} ${province}`;
+    copyright_ip.innerHTML = ip;
+    copyright_pos.innerHTML = pos;
+    copyright_isp.innerHTML = isp;
 })

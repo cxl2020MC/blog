a88b6cfdc0a6190cacc501d7298025c1f930d6e7
cxl2020MC <andamu2019@163.com>
2022-09-07 16:39:21 +0800

    git update

--- source/js/copyright_ip.js
+++ source/js/copyright_ip.js
@@ -4,6 +4,6 @@ fetch("https://api.cxl2020mc.top/ip/").then(res => res.json()).then(function (da
     city = data['data']['city'];
     province = data['data']['province'];
     isp = data['data']['isp'];
-    info = `你的IP: 【${ip}】 | 你所在的区域: 【${city} ${province}】 | 你的运营商: 【${isp}】`;
+    info = `你的IP: ${ip} | 你所在的区域: ${province} ${city} | 你的运营商: ${isp}`;
     copyright_ip.innerHTML = info;
 })

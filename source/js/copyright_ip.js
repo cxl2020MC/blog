26e399b70d3a72d8f91b54e3a67ccc5ecf9e9888
cxl2020MC <andamu2019@163.com>
2022-09-07 16:09:06 +0800

    git update

--- source/js/copyright_ip.js
+++ source/js/copyright_ip.js
@@ -1,13 +1,9 @@
 fetch("https://api.cxl2020mc.top/ip/").then(res => res.json()).then(function (data) {
     copyright_ip = document.getElementById("copyright_ip");
-    copyright_pos = document.getElementById("copyright_pos");
-    copyright_isp = document.getElementById("copyright_isp");
     ip = data['data']['ip'];
     city = data['data']['city'];
     province = data['data']['province'];
     isp = data['data']['isp'];
-    pos = `${ip} ${city} ${province}`;
+    pos = `你的IP: 【${ip}】 | 你所在的区域: 【${city} ${province}】 | 你的运营商: 【${isp}】`;
     copyright_ip.innerHTML = ip;
-    copyright_pos.innerHTML = pos;
-    copyright_isp.innerHTML = isp;
 })
